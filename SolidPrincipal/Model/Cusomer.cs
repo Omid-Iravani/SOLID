ae80a3570a496697d4bee41e3b7b832dfa62613d
omid iravani <soar.omid@gmail.com>
2017-11-27 11:39:03 +0330

    Understanding “S”- SRP (Single responsibility principle)

--- SolidPrincipal/Model/Cusomer.cs
+++ SolidPrincipal/Model/Cusomer.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SolidPrincipal.Model
+namespace Model
 {
    public class Cusomer
     {
+        //So SRP says that a class should have only one responsibility and not multiple.
+        private FileLogger obj = new FileLogger();
+
+        public virtual void Add()
+        {
+            try
+            {
+                // Database code goes here
+            }
+            catch (Exception ex)
+            {
+                obj.Handle(ex.ToString());
+            }
+        }
     }
 }
